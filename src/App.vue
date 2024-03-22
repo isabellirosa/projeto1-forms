48d386628fd92367328eaab2c1ed94807e5e6cc9
isabellirosa <isabelli.ifc@gmail.com>
2024-03-22 13:55:57 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="main">
+    <form>
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
