109f9902cb6bc588ec19c5058e626af82c99619b
isabellirosa <isabelli.ifc@gmail.com>
2024-06-22 22:14:54 -0300

    feat: criação das variaveis e funçoes no script

--- src/App.vue
+++ src/App.vue
@@ -1,150 +1,230 @@
 <script setup>
+import { ref } from 'vue'
+const enviado=ref(false)
 
+function enviar() {
+  enviado.value = true
+}
+
+const usuario=ref(
+  {
+    nome:'',
+    sobrenome:'',
+    idade:'',
+    senha:'',
+    cep:'',
+    estado:'',
+    cidade:'',
+    biografia:'',
+    hobbies:[],
+    linguagem:'',
+    avatar:''    
+  }
+)
+const estados = [
+  { sigla: 'AC', nomeEstado: 'Acre' },
+  { sigla: 'AL', nomeEstado: 'Alagoas' },
+  { sigla: 'AP', nomeEstado: 'Amapá' },
+  { sigla: 'AM', nomeEstado: 'Amazonas' },
+  { sigla: 'BA', nomeEstado: 'Bahia' },
+  { sigla: 'CE', nomeEstado: 'Ceará' },
+  { sigla: 'DF', nomeEstado: 'Distrito Federal' },
+  { sigla: 'ES', nomeEstado: 'Espírito Santo' },
+  { sigla: 'GO', nomeEstado: 'Goiás' },
+  { sigla: 'MA', nomeEstado: 'Maranhão' },
+  { sigla: 'MT', nomeEstado: 'Mato Grosso' },
+  { sigla: 'MS', nomeEstado: 'Mato Grosso do Sul' },
+  { sigla: 'MG', nomeEstado: 'Minas Gerais' },
+  { sigla: 'PA', nomeEstado: 'Pará' },
+  { sigla: 'PB', nomeEstado: 'Paraíba' },
+  { sigla: 'PR', nomeEstado: 'Paraná' },
+  { sigla: 'PE', nomeEstado: 'Pernambuco' },
+  { sigla: 'PI', nomeEstado: 'Piauí' },
+  { sigla: 'RJ', nomeEstado: 'Rio de Janeiro' },
+  { sigla: 'RN', nomeEstado: 'Rio Grande do Norte' },
+  { sigla: 'RS', nomeEstado: 'Rio Grande do Sul' },
+  { sigla: 'RO', nomeEstado: 'Rondônia' },
+  { sigla: 'RR', nomeEstado: 'Roraima' },
+  { sigla: 'SC', nomeEstado: 'Santa Catarina' },
+  { sigla: 'SP', nomeEstado: 'São Paulo' },
+  { sigla: 'SE', nomeEstado: 'Sergipe' },
+  { sigla: 'TO', nomeEstado: 'Tocantins' }
+]
+function handleFileUpload(e) {
+  const target = e.target
+  console.log(target)
+  if (target && target.files) {
+    const file = target.files[0]
+    usuario.value.avatar = URL.createObjectURL(file)
+  }
+}
 </script>
 
 <template>
   <div class="main">
+    <div v-if="enviado">
+      <img v-if="usuario.avatar" class="avatar" :src="usuario.avatar" />
+      <p v-for="(value, key) of usuario" :key="key">{{ key }}: {{ value }}</p>
+    </div>
+    <div v-else>
     <img src="../src/perfil (2).png" width="40px">
     <h1>Perfil</h1>
-    <form class="row g-4">
-  <div class="col-md-5">
-    <input type="text" class="form-control" id="inputEmail4" placeholder="nome">
-  </div>
-  <div class="col-md-5">
-    <input type="text" class="form-control" id="inputPassword4" placeholder="sobrenome">
-  </div>
-  <div class="col-md-2">
-    <input type="number" class="form-control" id="inputPassword4" placeholder="idade">
-  </div>
-  <div class="col-4">
-    <input type="email" class="form-control" id="inputEmail4" placeholder="email">
-  </div>
-  <div class="col-md-4">
-    <input type="password" class="form-control" id="inputPassword4" placeholder="insira sua senha">
-  </div>
-  <div class="col-md-4">
-    <input type="password" class="form-control" id="inputPassword4" placeholder=" confirme sua senha">
-  </div>
-  <div class="col-md-2">
-    <input type="text" class="form-control" id="inputZip"  placeholder="CEP">
-  </div>
-  <div class="col-md-4">
-    <select id="inputState" class="form-select"  placeholder="estado">
-      <option selected>Estado</option>
-      <option>...</option>
-    </select>
-  </div> 
-  <div class="col-md-6">
-    <input type="text" class="form-control" id="inputCity"  placeholder="cidade">
-  </div>
-  
-  <div class="col-4">
-    <textarea cols="35" rows="6" placeholder="Biografia"/>
-  </div>
-  <div class="col-2 ">
-    <h1 class="categorias">Hobbies:</h1>
-    <div class="form-check">
-  <input class="form-check-input" type="checkbox" name="esportes" id="esportes">
-  <label class="form-check-label" for="esportes">
-    Esportes
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="checkbox" name="ler" id="ler">
-  <label class="form-check-label" for="ler">
-    Ler
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="checkbox" name="esportes" id="esportes">
-  <label class="form-check-label" for="esportes">
-    Esportes
-  </label>
-</div>
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" name="esportes" id="esportes">
-  <label class="form-check-label" for="esportes">
-    Esportes
-  </label>
-</div>
-  </div>
-  
-  <div class="col-2 ">
-    <h1 class="categorias">Best language:</h1>
-    <div class="form-check">
-  <input class="form-check-input" type="radio" name="esportes" id="esportes">
-  <label class="form-check-label" for="esportes">
-    Esportes
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="ler" id="ler">
-  <label class="form-check-label" for="ler">
-    Ler
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="esportes" id="esportes">
-  <label class="form-check-label" for="esportes">
-    Esportes
-  </label>
-</div>
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="esportes" id="esportes">
-  <label class="form-check-label" for="esportes">
-    Esportes
-  </label>
-</div>
-  </div>
-  <div class="col-4">
-    <div class="form-check">
-      <label class="form-check-label" for="inputGroupFile01">Foto de perfil:</label>
-      <input type="file" class="form-control" id="inputGroupFile01">
-      <button type="submit" class="btn btn-primary">Enviar</button>
-    </div>
-  </div>
-</form>
-  </div>
-</template>
+    <form @submit.prevent="enviar()" validate class="row g-4">
+      <div class="col-md-5">
+        <input v-model="usuario.nome" type="text" class="form-control" id="inputEmail4" placeholder="nome">
+      </div>
+      <div class="col-md-5">
+        <input v-model="usuario.sobrenome" type="text" class="form-control" id="inputPassword4" placeholder="sobrenome">
+      </div>
+      <div class="col-md-2">
+        <input v-model="usuario.idade" type="number" class="form-control" id="inputPassword4" placeholder="idade">
+      </div>
+      <div class="col-4">
+        <input v-model="usuario.email" type="email" class="form-control" id="inputEmail4" placeholder="email">
+      </div>
+      <div class="col-md-4">
+        <input v-model="usuario.senha" type="password" class="form-control" id="inputPassword4" placeholder="insira sua senha">
+      </div>
+      <div class="col-md-4">
+        <input type="password" class="form-control" id="inputPassword4" placeholder=" confirme sua senha">
+      </div>
+      <div class="col-md-2">
+        <input v-model="usuario.cep" type="text" class="form-control" id="inputZip" placeholder="CEP">
+      </div>
+      <div class="col-md-4">
+        <select v-model="usuario.estado" id="inputState" class="form-select" placeholder="estado">
+          <option selected disabled>Estado</option>
+          <option v-for="option in estados" :key="option">
+            {{ option.sigla }}
+          </option>
+        </select>
+      </div>
+      <div class="col-md-6">
+        <input v-model="usuario.cidade" type="text" class="form-control" id="inputCity" placeholder="cidade">
+      </div>
 
+      <div class="col-4">
+        <textarea v-model="usuario.biografia" cols="35" rows="6" placeholder="Biografia" class="form-control" />
+      </div>
+      <div class="col-2 ">
+        <h1 class="categorias">Hobbies:</h1>
+        <div class="form-check">
+          <input v-model="usuario.hobbies" class="form-check-input" type="checkbox" name="esportes" value="Esportes">
+          <label class="form-check-label" for="esportes">
+            Esportes
+          </label>
+        </div>
+        <div class="form-check">
+          <input v-model="usuario.hobbies" class="form-check-input" type="checkbox" name="ler" value="Ler">
+          <label class="form-check-label" for="ler">
+            Ler
+          </label>
+        </div>
+        <div class="form-check">
+          <input v-model="usuario.hobbies" class="form-check-input" type="checkbox" name="cozinhar" value="Cozinhar">
+          <label class="form-check-label" for="cozinhar">
+            Cozinhar
+          </label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input v-model="usuario.hobbies" class="form-check-input" type="checkbox" name="games" value="Games">
+          <label class="form-check-label" for="games">
+            Games
+          </label>
+        </div>
+      </div>
+
+      <div class="col-2 ">
+        <h1 class="categorias">Best language:</h1>
+        <div class="form-check">
+          <input v-model="usuario.linguagem" class="form-check-input" type="radio" name="language" value="Python">
+          <label class="form-check-label" for="language">
+            Python
+          </label>
+        </div>
+        <div class="form-check">
+          <input v-model="usuario.linguagem" class="form-check-input" type="radio" name="language" value="PHP">
+          <label class="form-check-label" for="language">
+            PHP
+          </label>
+        </div>
+        <div class="form-check">
+          <input v-model="usuario.linguagem" class="form-check-input" type="radio" name="language" value="Java">
+          <label class="form-check-label" for="language">
+            Java
+          </label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input v-model="usuario.linguagem" class="form-check-input" type="radio" name="language" value="Javascript">
+          <label class="form-check-label" for="language">
+            Javascript
+          </label>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="form-check">
+          <label class="form-check-label" for="inputGroupFile01">Foto de perfil:</label>
+          <input type="file" class="form-control" id="inputGroupFile01" @change="handleFileUpload($event)" >
+          <button @click="enviar()" type="submit" class="btn btn-primary">Enviar</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+</template>
+"
 <style scoped>
-h1{
-  color:white;
+h1 {
+  color: white;
   display: inline;
 }
-img{
-  margin-bottom:20px;
+
+img {
+  margin-bottom: 20px;
   margin-right: 13px;
 }
-.categorias{
-  color:rgb(59, 59, 59);
+
+.categorias {
+  color: rgb(59, 59, 59);
   font-size: medium;
 }
-.biografia{
+
+.biografia {
   height: 300px;
 }
-.main{
-  position: absolute; 
-  top: 50%; 
-  left: 50%; 
-  transform: translate(-50%, -50%); 
+
+.main {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   padding: 30px;
   border: 3px solid white;
   border-radius: 10px;
   background-color: rgba(240, 248, 255, 0.459);
   width: 1000px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 }
-#inputGroupFile01{
+
+#inputGroupFile01 {
   margin-top: 10px;
 }
-.btn{
-  width:170px;
+
+.btn {
+  width: 170px;
+  height: 40px;
   border-radius: 10px;
   border: 2px solid white;
   color: rgb(41, 40, 40);
-  font-weight:500;
+  font-weight: 500;
   background-color: rgba(195, 81, 247, 0.459);
-  margin-top:20px;
+  margin-top: 20px;
   margin-left: 20%;
+}
+
+.btn:hover {
+  width: 200px;
+  height: 50px;
+  background-color: rgba(218, 139, 255, 0.459);
 }
 </style>
